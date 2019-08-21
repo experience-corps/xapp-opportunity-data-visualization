c052f1fc8876c858a6a3606c1376df971e4b7a6b
Christopher Tso <christopher.tso@Christophers-MacBook-Pro.local>
2019-08-21 14:08:27 -0500

    Created array of opp options

---
+++ src/components/oppsOptions.js
@@ -0,0 +1,3 @@
+var uniqueOpps = ["Banner - 25 Premium", "Connexall- New +50 ESS", "Dupe", "Eads Fence Company- PLU + 10", "Eaton Electrical Sector Pilot +120 PREM", 
+"ENGIE Insight - Content upgrade with LC renewal", "Honeywell Building Solutions +202ENT", "New - 1E - PLU + 100", "New - AAA National", "New - BitGo, Inc", "New - BMW BeLux ULT +12 +UNL PACK"]
+module.exports = uniqueOpps;
