a275e2b08a8819f70a55490ec7df0ef9ca483222
Christopher Tso <christopher.tso@Christophers-MacBook-Pro.local>
2019-08-21 13:34:34 -0500

    Added vuetify plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Select from './components/select.vue'
+import vuetify from './plugins/vuetify.js'
 
 new Vue({
     el: '#app', 
+    vuetify,
     render: h => h(Select)
 })
