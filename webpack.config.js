2546576307cfdbc71aecfd19280bdede73c42d5e
Christopher Tso <christopher.tso@Christophers-MacBook-Pro.local>
2019-08-21 13:29:04 -0500

    Added new rule

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,11 +7,27 @@ module.exports = {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
       { test: /\.vue$/, use: 'vue-loader' },
-      { test: /\.css$/, use: ['vue-style-loader', 'css-loader']} 
+      { test: /\.css$/, use: ['vue-style-loader', 'css-loader']}, 
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              fiber: require('fibers'),
+              indentedSyntax: true // optional
+            }
+          }
+        ]
+      }  
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(), 
+    new VuetifyLoaderPlugin()
   ],
   output: {
       filename: 'bundle.js', 
