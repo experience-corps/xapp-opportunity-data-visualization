185d5e9a62799f9207fb4abaac3778e51981f334
Christopher Tso <christopher.tso@Christophers-MacBook-Pro.local>
2019-08-22 14:54:09 -0500

    Added fusioncharts and router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
-import Select from './components/select.vue'
+import View from './components/view.vue'
+import router from './components/router'
 import vuetify from './plugins/vuetify.js'
+import VueFusionCharts from 'vue-fusioncharts'
+import FusionCharts from 'fusioncharts'
+import Gantt from 'fusioncharts/fusioncharts.gantt'
+import Bubble from 'fusioncharts/fusioncharts.charts'
+import Fusion from 'fusioncharts/themes/fusioncharts.theme.fusion'
+
+Vue.use(VueFusionCharts, FusionCharts, Gantt, Bubble, Fusion);
 
 new Vue({
     el: '#app', 
+    router,
     vuetify,
-    render: h => h(Select)
+    render: h => h(View)
 })
