e6eea0643eecf3d64f111cb17e10d10e7a44bf8e
Christopher Tso <christopher.tso@Christophers-MacBook-Pro.local>
2019-08-22 16:27:08 -0500

    Updated options

--- src/components/oppsOptions.js
+++ src/components/oppsOptions.js
@@ -1,3 +1,6 @@
 var uniqueOpps = ["Banner - 25 Premium", "Connexall- New +50 ESS", "Dupe", "Eads Fence Company- PLU + 10", "Eaton Electrical Sector Pilot +120 PREM", 
-"ENGIE Insight - Content upgrade with LC renewal", "Honeywell Building Solutions +202ENT", "New - 1E - PLU + 100", "New - AAA National", "New - BitGo, Inc", "New - BMW BeLux ULT +12 +UNL PACK"]
+"ENGIE Insight - Content upgrade with LC renewal", "Honeywell Building Solutions +202ENT", "New - 1E - PLU + 100", "New - AAA National", "New - BitGo, Inc", "New - BMW BeLux ULT +12 +UNL PACK", 
+"Athlon Carlease International +400ESS 1Y", "Carl Zeiss Vision International GmbH +1000ULT 1Y", "Connexall- New +50 ESS", "Demo Oppty - SFDC Integration", "Emerson Commercial & residential EMEA (Ridge Tool Europe) +70PREM 1Y", 
+"Envivent - biz dev", "Harman Lifestyle Audio +100 (880) ULT", "Honeywell Building Solutions +202ENT", "Infineum UK - 50 Prem", "New - Alysium tech GmbH PLUS - 10 + Distributors",
+"New - Indeed ESS - 200(2000)", "New - Coilcraft +30"];
 module.exports = uniqueOpps;
